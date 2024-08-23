15a7784d9a92fdf44351f58f467e69a2e88dec1c
Axel W <axelwestman@outlook.com>
2024-08-23 11:51:44 -0300

    Agregado de componente Navbar

---
+++ src/Components/Header_component.jsx
@@ -0,0 +1,11 @@
+import React from  'react';
+
+const Header_component = () => {
+
+    return (
+        <h1 className='text-red-700'>Hola Mudo</h1>
+
+    );
+}
+
+export default Header_component;
