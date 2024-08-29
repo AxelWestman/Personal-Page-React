1431bc4dfc156961cb820c1e6ee864e5c53c8755
Axel W Notebook <axelwestman@outlook.com>
2024-08-29 18:53:26 -0300

    agrego efecto typeado

--- src/Components/Header_component.jsx
+++ src/Components/Header_component.jsx
@@ -1,5 +1,6 @@
 import React from  'react';
 import MePhoto from '../assets/images/IMG_20230522_220407_null~2-Photoroom.png'
+import Typewriter from 'typewriter-effect';
 
 const Header_component = () => {
 
@@ -10,7 +11,21 @@ const Header_component = () => {
                 <img src={MePhoto} alt="Me" className='object-contain overflow-hidden'/>
             </div>
                 <h1 className='font-baskerville text-7xl text-white'>Axel Westman</h1>
-                <h3 className='font-baskerville text-3xl text-white'>Web Developer</h3>
+                <h3 className='font-baskerville text-3xl text-white float-left relative'>
+                    <Typewriter
+                        options={{
+                        autoStart: true,
+                        loop: true,
+                      }}
+                        onInit={typewriter => {
+                            typewriter
+                            .typeString('Web Developer')
+                            .pauseFor(2000)
+                            .deleteAll()
+                            .start()
+                        }}
+                    />
+                </h3>
             </div>
         </>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,25 @@ module.exports = {
     ],
   theme: {
     extend: {
+      animation: {
+        'escribir' : 'escribir 5s steps(13) infinite alternate',
+      },
+      keyframes:{
+        escribir: {
+          'from': { width: '100%' },
+          'to': { width: '0' }
+      },
+    },
       backgroundImage: {
         'bootstrap-photo': "url('src/assets/images/bootstrap_page.png')",
         'tailwind-photo': "url('src/assets/images/tailwind-project.png')",
         'react-photo': "url('src/assets/images/react-photo.png')",
-        'spacex': "url('src/assets/images/spacex.png')"
+        'spacex': "url('src/assets/images/spacex.png')",
+        'gradient-bg': {
+          'background-image': 'linear-gradient(135deg, hsla(350, 100%, 31%, 1) 0%, hsla(229, 56%, 20%, 1) 87%)',
+          'background-size': 'cover',
+          'background-position': 'center'
+        }
       },
       fontFamily:{
           baskerville:["Libre Baskerville", "serif"],
