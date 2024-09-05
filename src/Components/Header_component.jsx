2444aa4893bda07d1c53582a467c9da0509f7cfd
Axel W <axelwestman@outlook.com>
2024-09-05 15:29:13 -0300

    añadido de foto

--- src/Components/Header_component.jsx
+++ src/Components/Header_component.jsx
@@ -1,5 +1,5 @@
 import React from  'react';
-import MePhoto from '../assets/images/IMG_20230522_220407_null~2-Photoroom.png'
+import MePhoto from '../assets/images/foto-perfil.png'
 import Typewriter from 'typewriter-effect';
 
 const Header_component = () => {
@@ -7,8 +7,8 @@ const Header_component = () => {
     return (
         <>
             <div className='w-full h-lvh flex flex-col text-center items-center justify-center' id="header">
-            <div className='w-52 h-52 border rounded-full flex flex-col mb-4'>
-                <img src={MePhoto} alt="Me" className='object-contain overflow-hidden'/>
+            <div className='w-72 h-72  rounded-full flex flex-col mb-4 items-center  justify-center overflow-hidden'>
+                <img src={MePhoto} alt="Me" className='w-60 '/>
             </div>
                 <h1 className='font-baskerville text-7xl text-white'>Axel Westman</h1>
                 <h3 className='font-baskerville text-3xl text-white float-left relative'>
