100cf717b8e0cc64918193dade1f1cfe35f15aaa
Axel W Notebook <axelwestman@outlook.com>
2024-09-06 13:15:59 -0300

    efectos hover

--- src/Components/About_component.jsx
+++ src/Components/About_component.jsx
@@ -62,7 +62,7 @@ const About_component = () => {
                     <h2 className='text-center text-white text-3xl font-baskerville md:hidden'>Sobre mi</h2>
                     <p className='text-2xl font-baskerville text-white mt-4'>Hey there! My name is Axel, and i'm a junior web developer based in Santa Fe, Argentina. I work with some of the popular front end technologies to create beautiful websites that get noticed. I seek experience to gain more knowledge in the web development field.
                        If you are in need of a beautiful simple website, I'm your guy. I look forward to talking to you soon!</p>
-                       <div className='mt-4 w-48 h-14 bg-gray-700 text-white font-baskerville rounded-md flex justify-center items-center gap-3 shadow-2xl shadow-blue-500/20'>
+                       <div className='mt-4 w-48 h-14 bg-gray-700 text-white font-baskerville rounded-md flex justify-center items-center gap-3 shadow-2xl shadow-blue-500/20 transform transition duration-500 hover:scale-105'>
                         <img src={cv} alt="github logo" className='w-9'/>
                         <h2 className='text-xl mt-1'>Currículum</h2>
                     </div>
