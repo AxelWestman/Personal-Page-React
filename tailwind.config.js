f2e24cefd7d75ab6e966fbf6b20759cacb0eb471
Axel W <axelwestman@outlook.com>
2024-09-10 22:35:34 -0300

    resolución problema rutas fotos

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,12 +29,12 @@ module.exports = {
       },
     },
       backgroundImage: {
-        'bootstrap-photo': "url('src/assets/images/bootstrap_page.png')",
-        'tailwind-photo': "url('src/assets/images/tailwind-project.png')",
-        'react-photo': "url('src/assets/images/react-photo.png')",
-        'spacex': "url('src/assets/images/spacex.png')",
-        'php-project': "url('src/assets/images/php_page.png')",
-        "calculator-react": "url('src/assets/images/calculadora.png')"
+        'bootstrap-photo': "url('/src/assets/images/bootstrap_page.png')",
+        'tailwind-photo': "url('/src/assets/images/tailwind-project.png')",
+        'react-photo': "url('/src/assets/images/react-photo.png')",
+        'spacex': "url('/src/assets/images/spacex.png')",
+        'php-project': "url('/src/assets/images/php_page.png')",
+        "calculator-react": "url('/src/assets/images/calculadora.png')"
       },
       fontFamily:{
           baskerville:["Libre Baskerville", "serif"],
